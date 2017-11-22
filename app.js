66adb26041653f0b02d25e6807ec346b1aceca0a
Leo Majowka <leo.majowka@gmail.com>
2017-11-22 14:36:17 +0200

    remove logger

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 let glob = 1;
-let logger = require('./logger');
+
 var path = require('path');
 
 app.use(express.static('components'));
-app.use(logger);
+
 
 let index = require('./routes/index');
 app.use('/', index);
