f3e1298cb88086dec613d70f6107ef15cfebe3d4
Leo Majowka <leo.majowka@gmail.com>
2017-11-22 13:36:12 +0200

    render a simple view

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var express = require('express');
 var app = express();
 let glob = 1;
 let logger = require('./logger');
+var path = require('path');
 
 app.use(express.static('public'));
 app.use(logger);
 
 let index = require('./routes/index');
 app.use('/', index);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'slm');
+
+app.use(express.static(__dirname + '/views'));
 
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var path = require('path');
 
 
 router.route('/').get(function (request, response){
-  response.end();
+  response.sendFile('index.html', { root: path.resolve(__dirname, '../views') });
 });
 
 module.exports = router;
