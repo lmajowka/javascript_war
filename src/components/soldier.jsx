1f69a8605ab7102c886b6781a1b4e007d44984ef
Leo Majowka <leo.majowka@gmail.com>
2017-11-26 14:14:04 +0200

    on click change color state to green

--- src/components/soldier.jsx
+++ src/components/soldier.jsx
@@ -1,5 +1,13 @@
 class Soldier extends React.Component {
 
+  constructor(){
+    super();
+    this.state = {
+      color: '#F00'
+    }
+  }
+
+
   render() {
 
     let style = {
@@ -7,9 +15,13 @@ class Soldier extends React.Component {
       left: this.props.x + 'px'
     }
 
+    let color = {
+      color: this.state.color
+    }
+
     return (
-      <div style={style} class='soldier'>
-        <div class='soldier-body'>
+      <div style={style} class='soldier' onClick={this.onItemClick.bind(this)}>
+        <div class='soldier-body' style={color}>
           *
         </div>
         <div>  
@@ -18,4 +30,9 @@ class Soldier extends React.Component {
     </div>
     );
   }
+
+  onItemClick(){
+    this.setState({color: '#0F0'});
+  }
+
 }
